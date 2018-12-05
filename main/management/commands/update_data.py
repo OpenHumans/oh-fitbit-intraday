522063145faaa89bd8299f5a476547dd14e46bb0
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-12-04 16:01:28 -0800

    add management task for update data

---
+++ main/management/commands/update_data.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from openhumans.models import OpenHumansMember
+from main import helpers
+import requests
+from main.tasks import update_fitbit
+
+
+class Command(BaseCommand):
+    help = 'Process so far unprocessed data sets'
+
+    def handle(self, *args, **options):
+        for oh_member in OpenHumansMember.objects.all():
+            if hasattr(oh_member, 'fitbituser'):
+                update_fitbit(oh_member.oh_id)
