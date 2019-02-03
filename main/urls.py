560fafd76458d8eaaa36634150c1ad5969392b81
Mad Price Ball <mpball@gmail.com>
2019-02-03 15:01:05 -0800

    Update user flow and add more pages

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^logout/?$', views.logout_user, name='logout'),
-    url(r'^about/?$', views.about, name='about'),
-    url(r'^create-fitbit/?$', views.create_fitbit, name='create-fitbit'),
-    url(r'^delete-fitbit/?$', views.delete_fitbit, name='delete-fitbit'),
-    url(r'^fitbit/authorized/?$', views.complete_fitbit, name='fb_complete'),
+    path(r'', views.index, name='index'),
+    path(r'setup/', views.setup, name='setup'),
+    path(r'dashboard/', views.dashboard, name='dashboard'),
+    path(r'logout/', views.logout_user, name='logout'),
+    path(r'about/', views.about, name='about'),
+    path(r'create-fitbit/', views.create_fitbit, name='create-fitbit'),
+    path(r'delete-fitbit/', views.delete_fitbit, name='delete-fitbit'),
+    path(r'fitbit/authorized/', views.complete_fitbit, name='fb_complete'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import logout
-from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import redirect, render
-from django.utils.safestring import mark_safe
 import base64
 import requests
 from openhumans.models import OpenHumansMember
@@ -11,50 +9,78 @@ from .models import FitbitUser
 from .tasks import update_fitbit
 
 
+def _fitbit_is_setup(request):
+    """
+    Return True if user is logged in and Fitbit is set up.
+    """
+    if request.user.is_authenticated:
+        if hasattr(request.user.openhumansmember, 'fitbituser'):
+            if request.user.openhumansmember.fitbituser.access_token:
+                return True
+    return False
+
+
 def index(request):
     """
     Starting page for app.
     """
-    try:
-        auth_url = OpenHumansMember.get_auth_url()
-    except ImproperlyConfigured:
-        auth_url = None
-    if not auth_url:
-        messages.info(request,
-                      mark_safe(
-                          '<b>You need to set up your ".env"'
-                          ' file!</b>'))
-
-    context = {'auth_url': auth_url}
     if request.user.is_authenticated:
-        context['fb_redirect_uri'] = (
-            settings.OPENHUMANS_APP_BASE_URL+'/'
-            'fitbit/authorized')
-        if hasattr(request.user.openhumansmember, 'fitbituser'):
-            context['fitbituser'] = request.user.openhumansmember.fitbituser
-            if not request.user.openhumansmember.fitbituser.access_token:
-                fb_user = request.user.openhumansmember.fitbituser
-                fb_auth_url = (
-                 'https://www.fitbit.com/oauth2/authorize?response_type=code'
-                 '&client_id='+fb_user.personal_client_id+'&scope'
-                 '=activity%20nutrition%20heartrate%20location'
-                 '%20nutrition%20profile%20settings%20sleep%20social%20weight'
-                 '&redirect_uri='+settings.OPENHUMANS_APP_BASE_URL+'/'
-                 'fitbit/authorized')
-                context['fb_auth_url'] = fb_auth_url
+        if _fitbit_is_setup(request):
+            return redirect('dashboard')
+        return redirect('setup')
+
+    auth_url = OpenHumansMember.get_auth_url()
+    context = {'auth_url': auth_url}
+
     return render(request, 'main/index.html', context=context)
+
+
+def setup(request):
+    """
+    Set up Fitbit connection with API keys.
+    """
+    if not request.user.is_authenticated:
+        return redirect('index')
+
+    context = {
+        'fb_redirect_uri': (
+            settings.OPENHUMANS_APP_BASE_URL + '/fitbit/authorized')
+    }
+
+    if hasattr(request.user.openhumansmember, 'fitbituser'):
+        context['fitbituser'] = request.user.openhumansmember.fitbituser
+        if not request.user.openhumansmember.fitbituser.access_token:
+            fb_user = request.user.openhumansmember.fitbituser
+            fb_auth_url = (
+                'https://www.fitbit.com/oauth2/authorize?response_type=code'
+                '&client_id='+fb_user.personal_client_id+'&scope'
+                '=activity%20nutrition%20heartrate%20location'
+                '%20nutrition%20profile%20settings%20sleep%20social%20weight'
+                '&redirect_uri='+settings.OPENHUMANS_APP_BASE_URL+'/'
+                'fitbit/authorized')
+            context['fb_auth_url'] = fb_auth_url
+
+    return render(request, 'main/setup.html', context=context)
+
+
+def dashboard(request):
+    """
+    Show files/status dashboard for user that has completed setup.
+    """
+    context = {'data_files': request.user.openhumansmember.list_files()}
+    return render(request, 'main/dashboard.html', context=context)
 
 
 def about(request):
     """
-    give FAQ and further details on the app
+    Share further details about the project.
     """
     return render(request, 'main/about.html')
 
 
 def logout_user(request):
     """
-    Logout user
+    Logout user.
     """
     if request.method == 'POST':
         logout(request)
@@ -64,7 +90,7 @@ def logout_user(request):
 
 def create_fitbit(request):
     """
-    collect fitbit client id/Secret
+    Collect fitbit client id/secret.
     """
     if request.method == 'POST':
         if hasattr(request.user.openhumansmember, 'fitbituser'):
@@ -81,6 +107,9 @@ def create_fitbit(request):
 
 
 def delete_fitbit(request):
+    """
+    Delete fitbit client id/secret.
+    """
     if request.method == 'POST':
         fb = request.user.openhumansmember.fitbituser
         fb.delete()
@@ -88,6 +117,9 @@ def delete_fitbit(request):
 
 
 def complete_fitbit(request):
+    """
+    Complete OAuth2 set up with Fitbit authorization.
+    """
 
     code = request.GET['code']
 
@@ -97,9 +129,9 @@ def complete_fitbit(request):
     client_id = fb_user.personal_client_id
     client_secret = fb_user.personal_client_secret
     encode_fitbit_auth = str(client_id) + ":" + str(client_secret)
-    print(encode_fitbit_auth)
     b64header = base64.b64encode(
                     encode_fitbit_auth.encode("UTF-8")).decode("UTF-8")
+
     # Add the payload of code and grant_type. Construct headers
     payload = {
         'code': code,
@@ -109,13 +141,11 @@ def complete_fitbit(request):
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Basic %s' % (b64header)}
+
     # Make request for access token
     r = requests.post(
             'https://api.fitbit.com/oauth2/token', payload, headers=headers)
-    # print(r.json())
-
     rjson = r.json()
-    print(rjson)
 
     # Save the user as a FitbitMember and store tokens
     fb_user.user_id = rjson['user_id']
@@ -125,5 +155,7 @@ def complete_fitbit(request):
     fb_user.scope = rjson['scope']
     fb_user.token_type = rjson['token_type']
     fb_user.save()
+
+    # Start async processing and return user to dashboard
     update_fitbit.delay(fb_user.oh_member.oh_id)
-    return redirect('/')
+    return redirect('dashboard')
